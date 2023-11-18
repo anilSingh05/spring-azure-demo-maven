cad7bcc838ccedb47fff77e6ba1b1b4f1ab496fd
Anil Singh <anil05.me@gmail.com>
2023-11-18 14:56:00 -0600

    Initial Commit

---
+++ src/main/java/spring/azure/demo/maven/springazuredemomaven/SpringAzureDemoMavenApplication.java
@@ -0,0 +1,13 @@
+package spring.azure.demo.maven.springazuredemomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureDemoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/azure/demo/maven/springazuredemomaven/SpringAzureDemoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.azure.demo.maven.springazuredemomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
