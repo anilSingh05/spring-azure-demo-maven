6942143ab55770ec4f5fd105bf4b064456200b52
Anil Singh <anil05.me@gmail.com>
2023-11-19 20:17:50 -0600

    Added get Mapping

--- src/main/java/spring/azure/demo/maven/springazuredemomaven/SpringAzureDemoMavenApplication.java
+++ src/main/java/spring/azure/demo/maven/springazuredemomaven/SpringAzureDemoMavenApplication.java
@@ -2,10 +2,17 @@ package spring.azure.demo.maven.springazuredemomaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAzureDemoMavenApplication {
 
+	@GetMapping("/greetings")
+	public String greetings(){
+		return "Contratulation!! Your Service is up now";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureDemoMavenApplication.class, args);
 	}
